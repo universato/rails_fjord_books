d9765a2395650d39b66044e74c0b763dfb99454f
Yoshimine Sato <universato@gmail.com>
2020-08-17 22:31:44 +0900

    add author column

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_132601) do
+ActiveRecord::Schema.define(version: 2020_08_17_133023) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
   end
 
 end
