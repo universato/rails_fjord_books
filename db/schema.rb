55a3ac996e2e77ab562bbd56e9ed531f347cc33a
Yoshimine Sato <universato@gmail.com>
2020-08-17 22:44:56 +0900

    add picture and change language to Japanese

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_133023) do
+ActiveRecord::Schema.define(version: 2020_08_17_133235) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_08_17_133023) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
+    t.string "picture"
   end
 
 end
