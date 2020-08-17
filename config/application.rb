e855f8568b86a74a53491d174d85bb8800692dbe
Yoshimine Sato <universato@gmail.com>
2020-08-17 23:51:19 +0900

    change language to Japanese by using gem rails-i18n

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative "boot"
 
 require "rails/all"
@@ -15,5 +17,9 @@ module RailsFjordBooks
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
   end
 end
