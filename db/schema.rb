e4e4ed5fde763b8944d7e18223ab052e3a0f778e
Yoshimine Sato <universato@gmail.com>
2020-09-05 05:15:26 +0900

    delete extra empty line due to advice from RuboCop

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_09_04_033431) do
-
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -38,5 +37,4 @@ ActiveRecord::Schema.define(version: 2020_09_04_033431) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
-
 end
