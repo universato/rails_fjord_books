16d2c5e4d2e0427ddbc6dbaa808f4607351698e6
Yoshimine Sato <universato@gmail.com>
2020-09-09 13:10:39 +0900

    delete a extra parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@
 
 class ApplicationController < ActionController::Base
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :icon, :avatar])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :icon, :avatar])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :icon, :avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :icon])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :icon])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :icon])
   end
 end
