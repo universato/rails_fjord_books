96f7254fd0f20111373045b9e6f1327b7a9c4318
Yoshimine Sato <universato@gmail.com>
2020-09-13 19:07:52 +0900

    re-add picture to Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Book < ApplicationRecord
-  # mount_uploader :picture, PictureUploader
+  mount_uploader :picture, PictureUploader
   validates :title, presence: true
   paginates_per 10
 end
