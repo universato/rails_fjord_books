0226301b556e934c0b08b35cbd0e4e1789e01d4b
Yoshimine Sato <universato@gmail.com>
2020-09-22 21:45:17 +0900

    add url helper for seachability

--- app/controllers/user_relations_controller.rb
+++ app/controllers/user_relations_controller.rb
@@ -4,11 +4,11 @@ class UserRelationsController < ApplicationController
   def create
     user = User.find(params[:followed_id])
     current_user.follow(user)
-    redirect_to user
+    redirect_to user_path(user)
   end
   def destroy
     user = UserRelation.find(params[:id]).followed
     current_user.unfollow(user)
-    redirect_to user
+    redirect_to user_path(user)
   end
 end
