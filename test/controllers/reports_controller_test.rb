43634361ffbd44904016ae7386b90074e22519dc
Yoshimine Sato <universato@gmail.com>
2020-09-22 22:19:53 +0900

    add temporary files

--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -1,25 +1,3 @@
 # frozen_string_literal: true
 
-# require 'test_helper'
-
-# class ReportsControllerTest < ActionDispatch::IntegrationTest
-#   test "should get index" do
-#     get reports_index_url
-#     assert_response :success
-#   end
-
-#   test "should get show" do
-#     get reports_show_url
-#     assert_response :success
-#   end
-
-#   test "should get new" do
-#     get reports_new_url
-#     assert_response :success
-#   end
-
-#   test "should get edit" do
-#     get reports_edit_url
-#     assert_response :success
-#   end
-# end
+require "test_helper"
