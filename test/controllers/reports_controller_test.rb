581446d5dc90d7baac85a7683c8358a5981456f9
Yoshimine Sato <universato@gmail.com>
2020-09-22 21:34:28 +0900

    add test for validation and remove redundant validations

---
+++ test/controllers/reports_controller_test.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# require 'test_helper'
+
+# class ReportsControllerTest < ActionDispatch::IntegrationTest
+#   test "should get index" do
+#     get reports_index_url
+#     assert_response :success
+#   end
+
+#   test "should get show" do
+#     get reports_show_url
+#     assert_response :success
+#   end
+
+#   test "should get new" do
+#     get reports_new_url
+#     assert_response :success
+#   end
+
+#   test "should get edit" do
+#     get reports_edit_url
+#     assert_response :success
+#   end
+# end
--- test/models/user_relation_test.rb
+++ test/models/user_relation_test.rb
@@ -1,7 +1,22 @@
-require 'test_helper'
+# frozen_string_literal: true
+
+require "test_helper"
 
 class UserRelationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_normal
+    user1 = users(:one)
+    user2 = users(:two)
+    follow = UserRelation.new(follower_id: user1.id, followed_id: user2.id)
+    assert follow.valid?
+  end
+
+  def test_null_user
+    follow = UserRelation.new(follower_id: 1, followed_id: nil)
+    assert follow.invalid?
+  end
+  
+  def test_self_follow
+    follow = UserRelation.new(follower_id: 1, followed_id: 1)
+    assert follow.invalid?
+  end
 end
