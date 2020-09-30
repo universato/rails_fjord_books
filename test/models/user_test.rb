aa4c4d4b1ad74700c1e637dec06d00a34a69fc56
Yoshimine Sato <universato@gmail.com>
2020-09-30 10:59:48 +0900

    add model test and system test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,4 +7,16 @@ class UserTest < ActiveSupport::TestCase
     @user = users(:one)
     assert @user.valid?
   end
+
+  test "valid follow, unfollow, following?" do
+    @user = users(:one)
+    @other_user = users(:two)
+    assert_not @user.following?(@other_user)
+
+    @user.follow(@other_user)
+    assert @user.following?(@other_user)
+
+    @user.unfollow(@other_user)
+    assert_not @user.following?(@other_user)
+  end
 end
